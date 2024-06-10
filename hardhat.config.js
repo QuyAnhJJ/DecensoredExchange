e4817f79ae8f8e7f432beefeb42fd38349f77960
scev <minh.nn210582@sis.hust.edu.vn>
2024-06-10 12:43:47 +0700

    Project cleaning, installed some dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
-require("@nomiclabs/hardhat-ethers")
+const local_accounts = ["LOCAL_PK_0", "LOCAL_PK_1"].map(
+	(key) => process.env[key]
+);
+
 module.exports = {
-  solidity: "0.8.17",
+	solidity: "0.8.0",
+	networks: {
+		localhost: {
+			url: "http://127.0.0.1:8545",
+			local_accounts,
+		},
+	},
 };
