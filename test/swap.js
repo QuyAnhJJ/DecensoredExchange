57dd459ae46378e1807d22c2d9e50f6091c243f9
scev <minh.nn210582@sis.hust.edu.vn>
2024-06-11 12:19:43 +0700

    Normal ETH to tokens swap

--- test/swap.js
+++ test/swap.js
@@ -1,51 +1,65 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { ethers, network } = require("hardhat");
 
-describe("swap", function () {
-    let Token;
-    let token;
-    let TokenExchange;
-    let exchange;
-    let owner;
-    let addr1;
-    let addr2;
-  
-    beforeEach(async function () {
-      [owner, addr1, addr2] = await ethers.getSigners();
-  
-      // Deploy the Token contract
-      Token = await ethers.getContractFactory("Token");
-      token = await Token.deploy();
-      await token.deployed();
-  
-      // Deploy the TokenExchange contract
-      TokenExchange = await ethers.getContractFactory("TokenExchange");
-      exchange = await TokenExchange.deploy();
-      await exchange.deployed();
-  
-      // Mint tokens to the owner
-      await token.mint(owner.address, ethers.utils.parseUnits("1000", 18));
-    });
-  
-    describe("swapTokensForETH", function () {
-      it("Should swap tokens for ETH", async function () {
-        const initialTokenReserves = ethers.utils.parseUnits("1000", 18);
-        const initialEthReserves = ethers.utils.parseEther("1");
-  
-        // Create the liquidity pool
-        await exchange.createPool({ value: initialEthReserves });
-  
-        // Add liquidity
-        const additionalEth = ethers.utils.parseEther("0.5");
-        await exchange.addLiquidity(1, 100000, { value: additionalEth });
-  
-        // Swap tokens for ETH
-        const amountTokens = ethers.utils.parseUnits("10", 18);
-        const maxExchangeRate = 100000;
-        await expect(exchange.swapTokensForETH(amountTokens, maxExchangeRate))
-          .to.emit(token, "Transfer")
-          .withArgs(owner.address, exchange.address, amountTokens);
-      });
-    });
+describe("TokenExchange", function () {
+	let Token, token, TokenExchange, exchange, owner, addr1, addr2;
+	let token_reserves, eth_reserves;
+
+	const multiplier = ethers.BigNumber.from(10 ** 5);
+	const swap_fee_numerator = ethers.BigNumber.from(3);
+	const swap_fee_denominator = ethers.BigNumber.from(100);
+
+	beforeEach(async function () {
+		await network.provider.request({
+			method: "hardhat_reset",
+			params: [],
+		});
+
+		[owner, addr1, addr2] = await ethers.getSigners();
+
+		// Deploy the Token contract
+		Token = await ethers.getContractFactory("Token");
+		token = await Token.deploy();
+
+		// Deploy the TokenExchange contract
+		TokenExchange = await ethers.getContractFactory("TokenExchange");
+		exchange = await TokenExchange.deploy();
+
+		// Mint tokens to the owner
+		token_reserves = ethers.utils.parseUnits("5000", 18);
+		await token.mint(token_reserves);
+
+		// Approve the exchange contract to spend owner's tokens
+		await token.approve(exchange.address, token_reserves);
+
+		// Create the liquidity pool
+		eth_reserves = ethers.utils.parseUnits("5000", "ether");
+		await exchange.createPool(token_reserves, { value: eth_reserves });
+	});
+
+	describe("Swap ETH for Tokens", () => {
+		it("Normal swap ETH", async function () {
+			const ethAmount = ethers.utils.parseUnits("1", "ether");
+			await exchange.connect(addr1).swapETHForTokens(5, { value: ethAmount });
+
+			const bigEthReserves = ethers.BigNumber.from(eth_reserves.toString());
+
+			const bigTokenReserves = ethers.BigNumber.from(token_reserves);
+
+			const expectedAmountTokens = swap_fee_denominator
+				.sub(swap_fee_numerator)
+				.mul(ethAmount)
+				.mul(bigTokenReserves)
+				.div(bigEthReserves.add(ethAmount).mul(swap_fee_denominator));
+
+			expect(await ethers.provider.getBalance(exchange.address)).to.equal(
+				eth_reserves.add(ethAmount)
+			);
+
+			// Get the token balance of addr1
+			const tokenBalanceAfter = await token.balanceOf(addr1.address);
+			// Verify the token balance is as expected
+			expect(tokenBalanceAfter).to.equal(expectedAmountTokens);
+		});
+	});
 });
-  
